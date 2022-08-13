bb7fa71a0c25909e2515ef8cfda39e55c6c93f31
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2022-08-13 23:04:12 +0200

    Update IlVerifyTests.cs

--- BetterCallSodium.Tests/IlVerifyTests.cs
+++ BetterCallSodium.Tests/IlVerifyTests.cs
@@ -20,10 +20,12 @@ namespace BetterCallSodium.Tests
         public void VerifyTest()
         {
             int exitCode = ProcessUtils.GetProcessOutput("dotnet", "tool restore", out string output, out string error);
+
             if (!string.IsNullOrWhiteSpace(output))
                 _output.WriteLine($"Restore output: {output}");
             if (!string.IsNullOrWhiteSpace(error))
                 _output.WriteLine($"Restore error: {error}");
+
             Assert.Equal(0, exitCode);
 
             string references = "";
@@ -32,10 +34,12 @@ namespace BetterCallSodium.Tests
 
             string arguments = $"ilverify \"{typeof(MonoPInvokeCallbackAttribute).Assembly.Location}\" -s \"{typeof(object).Assembly.GetName().Name}\" {references}";
             exitCode = ProcessUtils.GetProcessOutput("dotnet", arguments, out output, out error);
+
             if (!string.IsNullOrWhiteSpace(output))
                 _output.WriteLine($"IlVerify output: {output}");
             if (!string.IsNullOrWhiteSpace(error))
                 _output.WriteLine($"IlVerify error: {error}");
+
             Assert.Equal(0, exitCode);
         }
     }
